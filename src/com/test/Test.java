b3d2a697a446802336fa421eba972b1260873254
Solomon Alexander <solomonalexander82@gmail.com>
2018-07-19 21:02:18 +1000

    draft version

---
+++ src/com/test/Test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+class Test {
+	protected int ting = 25;
+	
+	public static void main (String[] args) {
+		System.out.println("1");
+		
+	}
+
+}
---
+++ src/com/test/Test1.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Test1 extends Test{
+	
+	public Set getLocal() {
+		System.out.println(ting);
+		int[][] a1 = new int[5][4];
+		return new LinkedHashSet();
+	}
+
+}
